b76b65ad4214e563b79f1ab038820be9f266e545
Thanh Hoang-Minh <hmthanhgm@gmail.com>
2024-11-23 17:45:34 +0700

    update all

--- bvh2npy.py
+++ bvh2npy.py
@@ -1,19 +1,18 @@
-# This code was written by Simon Alexanderson
-# and is released here: https://github.com/simonalexanderson/PyMO
-
 import numpy as np
 import pandas as pd
 from sklearn.pipeline import Pipeline
+from concurrent.futures import ProcessPoolExecutor
 
 from argparse import ArgumentParser
 
 import glob
 import os
 import sys
-module_path = os.path.abspath(os.path.join('..'))
-if module_path not in sys.path:
-    sys.path.append(module_path)
+# module_path = os.path.abspath(os.path.join('..'))
+# if module_path not in sys.path:
+#     sys.path.append(module_path)
 
+import pymo
 from pymo.parsers import BVHParser
 from pymo.data import Joint, MocapData
 from pymo.preprocessing import *
@@ -23,69 +22,72 @@ from pymo.writers import *
 import joblib as jl
 import glob
 
-# 18 joints (only upper body)
-# target_joints = ['p_r_scap', 'p_l_scap', 'body_world', 'b_root', 'b_spine0', 'b_spine1', 'b_spine2', 'b_spine3', 'b_l_shoulder', 'b_l_arm', 'b_l_arm_twist', 'b_l_forearm', 'b_l_wrist_twist', 'b_l_wrist', 'b_r_shoulder', 'b_r_arm', 'b_r_arm_twist', 'b_r_forearm', 'b_r_wrist_twist', 'b_r_wrist', 'b_neck0', 'b_head']
-
-# 50 joints (upper body with fingers)
-# target_joints = ['p_r_scap', 'p_l_scap', 'body_world', 'b_root', 'b_spine0', 'b_spine1', 'b_spine2', 'b_spine3', 'b_l_shoulder', 'b_l_arm', 'b_l_arm_twist', 'b_l_forearm', 'b_l_wrist_twist', 'b_l_wrist', 'b_l_pinky1', 'b_l_pinky2', 'b_l_pinky3', 'b_l_ring1', 'b_l_ring2', 'b_l_ring3', 'b_l_middle1', 'b_l_middle2', 'b_l_middle3', 'b_l_index1', 'b_l_index2', 'b_l_index3', 'b_l_thumb0', 'b_l_thumb1', 'b_l_thumb2', 'b_l_thumb3', 'b_r_shoulder', 'b_r_arm', 'b_r_arm_twist', 'b_r_forearm', 'b_r_wrist_twist', 'b_r_wrist', 'b_r_thumb0', 'b_r_thumb1', 'b_r_thumb2', 'b_r_thumb3', 'b_r_pinky1', 'b_r_pinky2', 'b_r_pinky3', 'b_r_middle1', 'b_r_middle2', 'b_r_middle3', 'b_r_ring1', 'b_r_ring2', 'b_r_ring3', 'b_r_index1', 'b_r_index2', 'b_r_index3', 'b_neck0', 'b_head']
-
-# 24 joints (upper and lower body excluding fingers)
-# target_joints = ['body_world', 'b_root', 'b_l_upleg', 'b_l_leg', 'b_r_upleg', 'b_r_leg', 'b_spine0', 'b_spine1', 'b_spine2', 'b_spine3', 'b_l_shoulder', 'b_l_arm', 'b_l_arm_twist', 'b_l_forearm', 'b_l_wrist_twist', 'b_l_wrist', 'b_r_shoulder', 'b_r_arm', 'b_r_arm_twist', 'b_r_forearm', 'b_r_wrist_twist', 'b_r_wrist', 'b_neck0', 'b_head']
-
-#56 joints (upper and lower body including fingers)
-target_joints = ['p_r_scap', 'p_l_scap', 'body_world', 'b_root', 'b_l_upleg', 'b_l_leg', 'b_r_upleg', 'b_r_leg', 'b_spine0', 'b_spine1', 'b_spine2', 'b_spine3', 'b_l_shoulder', 'b_l_arm', 'b_l_arm_twist', 'b_l_forearm', 'b_l_wrist_twist', 'b_l_wrist', 'b_l_pinky1', 'b_l_pinky2', 'b_l_pinky3', 'b_l_ring1', 'b_l_ring2', 'b_l_ring3', 'b_l_middle1', 'b_l_middle2', 'b_l_middle3', 'b_l_index1', 'b_l_index2', 'b_l_index3', 'b_l_thumb0', 'b_l_thumb1', 'b_l_thumb2', 'b_l_thumb3', 'b_r_shoulder', 'b_r_arm', 'b_r_arm_twist', 'b_r_forearm', 'b_r_wrist_twist', 'b_r_wrist', 'b_r_thumb0', 'b_r_thumb1', 'b_r_thumb2', 'b_r_thumb3', 'b_r_pinky1', 'b_r_pinky2', 'b_r_pinky3', 'b_r_middle1', 'b_r_middle2', 'b_r_middle3', 'b_r_ring1', 'b_r_ring2', 'b_r_ring3', 'b_r_index1', 'b_r_index2', 'b_r_index3', 'b_neck0', 'b_head']
+target_joints = ["Hips", "Spine", "Spine1", "Spine2", "Spine3", "Neck", "Neck1", "Head", "HeadEnd", "RightShoulder", "RightArm", "RightForeArm", "RightHand", "RightHandThumb1", "RightHandThumb2", "RightHandThumb3", "RightHandThumb4", "RightHandIndex1", "RightHandIndex2", "RightHandIndex3", "RightHandIndex4", "RightHandMiddle1", "RightHandMiddle2", "RightHandMiddle3", "RightHandMiddle4", "RightHandRing1", "RightHandRing2", "RightHandRing3", "RightHandRing4", "RightHandPinky1", "RightHandPinky2",
+                 "RightHandPinky3", "RightHandPinky4", "RightForeArmEnd", "RightArmEnd", "LeftShoulder", "LeftArm", "LeftForeArm", "LeftHand", "LeftHandThumb1", "LeftHandThumb2", "LeftHandThumb3", "LeftHandThumb4", "LeftHandIndex1", "LeftHandIndex2", "LeftHandIndex3", "LeftHandIndex4", "LeftHandMiddle1", "LeftHandMiddle2", "LeftHandMiddle3", "LeftHandMiddle4", "LeftHandRing1", "LeftHandRing2", "LeftHandRing3", "LeftHandRing4", "LeftHandPinky1", "LeftHandPinky2", "LeftHandPinky3",
+                 "LeftHandPinky4", "LeftForeArmEnd", "LeftArmEnd", "RightUpLeg", "RightLeg", "RightFoot", "RightToeBase", "RightToeBaseEnd", "RightLegEnd", "RightUpLegEnd", "LeftUpLeg", "LeftLeg", "LeftFoot", "LeftToeBase", "LeftToeBaseEnd", "LeftLegEnd", "LeftUpLegEnd"]
 
 
-def extract_joint_angles(bvh_dir, files, dest_dir, pipeline_dir, fps):
+def process_file(file):
+    print("Processing", file)
+    p = BVHParser()
+    return p.parse(file)
+
+
+def extract_joint_angles(bvh_dir, files, npy_dir, pipeline_dir, fps):
     p = BVHParser()
 
     data_all = list()
     for f in files:
-        ff = f
-        print(ff)
-        data_all.append(p.parse(ff))
+        print("Processing ", f)
+        bvh_parsed = p.parse(f)
+        # frames = bvh_parsed.skeleton[:10]
+        # frames = bvh_parsed.values[:10]
+        data_all.append(bvh_parsed)
+
+    # with ProcessPoolExecutor() as executor:
+    #     data_all = list(executor.map(process_file, files))
 
     data_pipe = Pipeline([
-       ('dwnsampl', DownSampler(tgt_fps=30,  keep_all=False)),
-       #('root', RootNormalizer()),
-       ('jtsel', JointSelector(target_joints, include_root=False)),
-       ('pos', MocapParameterizer('position')),
-       ('np', Numpyfier())
+        # ('dwnsampl', DownSampler(rate=fps)),
+        # ('root', RootNormalizer()),
+        ('jtsel', JointSelector(target_joints, include_root=False)),
+        ('position', MocapParameterizer('position')),
+        ('np', Numpyfier())
     ])
-
-
+    #
     out_data = data_pipe.fit_transform(data_all)
-    
-    # the datapipe will append the mirrored files to the end
-    assert len(out_data) == len(files)
-    
-    jl.dump(data_pipe, os.path.join(pipeline_dir + 'data_pipe.sav'))
-        
-    fi=0
-    for f in files:
-        ff = f.split("/")[-1]
-        print(ff)
-        np.save(os.path.join(dest_dir, ff[:-4] + ".npy"), out_data[fi])
-        fi=fi+1
-
+    #
+    # # the data pipe will append the mirrored files to the end
+    # assert len(out_data) == len(files)
+    #
+    # jl.dump(data_pipe, os.path.join(pipeline_dir + 'data_pipe.sav'))
+    #
+    # fi = 0
+    # for f in files:
+    #     ff = f.split("/")[-1]
+    #     print(ff)
+    #     npy_file = os.path.join(npy_dir, ff[:-4] + ".npy")
+    #     np.save(npy_file, out_data[fi])
+    #     fi = fi + 1
 
 
 if __name__ == '__main__':
-
+    """
+    python bvh2npy.py --bvh_dir="./groundtruth/bvh" --npy_dir="./groundtruth/bvh" --pipeline_dir="./groundtruth"
+    """
     # Setup parameter parser
     parser = ArgumentParser(add_help=False)
     parser.add_argument('--bvh_dir', '-orig', required=True, default="./groundtruth/bvh",
-                                   help="Path where original motion files (in BVH format) are stored")
-    parser.add_argument('--dest_dir', '-dest', required=True, default="./groundtruth/npy",
-                                   help="Path where extracted motion features will be stored")
-    parser.add_argument('--pipeline_dir', '-pipe', default="./utils/",
+                        help="Path where original motion files (in BVH format) are stored")
+    parser.add_argument('--npy_dir', '-npy_dir', required=True, default="./groundtruth/npy",
+                        help="Path where extracted motion features will be stored")
+    parser.add_argument('--pipeline_dir', '-pipe', default="./groundtruth/",
                         help="Path where the motion data processing pipeline will be stored")
 
     params = parser.parse_args()
 
-    files = []
-    # Go over all BVH files
     print("Going to pre-process the following motion files:")
-    files = sorted([f for f in glob.iglob(params.bvh_dir+'/*.bvh')])
+    files = sorted([f for f in glob.iglob(params.bvh_dir + '/*.bvh')])[:2]
+    print("Total files: {}".format(len(files)))
 
-    extract_joint_angles(params.bvh_dir, files, params.dest_dir, params.pipeline_dir , fps=30)
+    extract_joint_angles(params.bvh_dir, files, params.npy_dir, params.pipeline_dir, fps=60)
